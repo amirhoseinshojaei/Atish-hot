e7211f838ecb78e1e14e42a079be698dd2914053
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-12-12 17:22:36 +0330

    build a custom user manager

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None):
+        if not phone_number:
+            raise ValueError('Users must have phone number')
+
+        if not password:
+            raise ValueError('Users must have password')
+
+        user = self.model(phone_number=phone_number)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # SuperUser Section
+    def create_superuser(self, phone_number, password):
+        user = self.create_user(phone_number, password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user

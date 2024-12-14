4c44f3d4000f10e80e20c85ff205ad3b9e17ba99
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-12-14 22:05:26 +0330

    build index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from . models import Products, CustomerReview
 # Create your views here.
+
+
+def index(request):
+    latest_products = Products.objects.order_by('-created_at')[:6]
+    suggested_products = Products.objects.filter(is_suggestion=True)[:6]
+    customer_reviews = CustomerReview.objects.order_by('-created_at')[:4]
+
+    return render(request, 'core/index.html', {
+        'latest_products': latest_products,
+        'suggested_products': suggested_products,
+        'customer_reviews': customer_reviews
+    })
+
+
